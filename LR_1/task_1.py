b14442a97efe1abb5058070b52f496885ba77284
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-09 14:05:50 +0300

    ля 1 задачка по ТОД :)

---
+++ LR_1/task_1.py
@@ -0,0 +1,10 @@
+a = int(input("Введите значение a: "))
+b = int(input("Введите значение b: "))
+print(f"{a} + {b} = {a+b}")
+print(f"{b}|{a}|{a+b}")
+print(f"Z({a}) = F({b})")
+print(f"x = {a}; y = {b}")
+print(f"Ответ: ({a}; {b})")
+
+
+
